46b9bc9b0a53b5a7b14af680d15d954c6e6b5eab
bieltomas <gtomasvidal@cifpfbmoll.eu>
2023-03-20 16:28:35 +0100

    final commit

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
-from .models import Pregunta
+from .models import Pregunta, Eleccion
 
-admin.site.register(Pregunta)
+class Opciones(admin.TabularInline):
+    model = Eleccion
+    extra = 1
+class PreguntaAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['textoPregunta']}),
+        ('Date information', {'fields': ['fechaPublicacion']}),
+    ]
+    inlines = [Opciones]
+    list_display = ('textoPregunta', 'fechaPublicacion', 'reciente')
+    list_filter = ['fechaPublicacion']
+    search_fields = ['textoPregunta']
+
+admin.site.register(Pregunta, PreguntaAdmin)
+
+
